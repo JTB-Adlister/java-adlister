1547813ab0430aa12b95dcf77f57ebf395600c8e
Thomas <trexmeyer@gmail.com>
2018-06-11 13:41:48 -0500

    css works

--- src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
+++ src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
@@ -1,14 +1,20 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Category;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/index")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("<h1>Hello, World!</h1>");
+        List<Category> categories = DaoFactory.getCategoriesDao().listAll();
+        request.getSession().setAttribute("categories", categories);
     }
 }
